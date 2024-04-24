bbeed3deae5b5014f734eab1e9597d2a4d62f9be
Cuong <cuong@foxit.co.jp>
2024-04-24 15:32:38 +0700

    add

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+ 
   reactStrictMode: true,
   experimental: {
   },
   images:{
+    path:"/",
     domains:['mayxaydungmoi.com']
   }
 }
